456e8f332bd26fdc34b685382e86fd56a043352e
rafapaezbas <rafaelpaezbastida@gmail.com>
2026-03-11 17:43:03 +0100

    defer'd registration

--- index.js
+++ index.js
@@ -7,44 +7,15 @@ module.exports = class MSIXManager {
     this._handle = binding.init()
   }
 
-  addPackage(file, opts = {}) {
-    return new MSIXManagerAddPackage(this, pathToFileURL(file).href, opts)
+  addPackage(file) {
+    return new MSIXManagerAddPackage(this, pathToFileURL(file).href)
   }
 }
 
-class MSIXManagerAddPackage extends EventEmitter {
-  constructor(manager, uri, opts) {
-    const { restartOnUpdate = false } = opts
-
-    super()
-
+class MSIXManagerAddPackage {
+  constructor(manager, uri) {
     this._manager = manager
 
-    this._completed = Promise.withResolvers()
-
-    this._handle = binding.addPackage(
-      manager._handle,
-      uri,
-      restartOnUpdate,
-      this,
-      this._onprogress,
-      this._oncompleted
-    )
-  }
-
-  then(resolve, reject) {
-    return this._completed.promise.then(resolve, reject)
-  }
-
-  _onprogress(progress) {
-    this.emit('progress', progress)
-  }
-
-  _oncompleted(err) {
-    if (err) {
-      this._completed.reject(new Error(err))
-    } else {
-      this._completed.resolve()
-    }
+    this._handle = binding.addPackage(manager._handle, uri, this)
   }
 }
